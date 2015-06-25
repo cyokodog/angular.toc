2e8b4a6c74a12178a688a91778372301ee268ead
cyokodog <cyokodog@gmail.com>
2015-06-26 00:09:06 +0900

    first commit

---
+++ angular.toc.js
@@ -0,0 +1,36 @@
+//toc.module.js
+;(function(){
+  angular.module('angular-toc', []);
+})();
+
+//toc.directive.js
+;(function(){
+  angular.module('angular-toc').directive('toc', directiveFactory);
+  function directiveFactory(){
+    return {
+      scope: {
+        startHeadingElement: '@',
+        fromHeadingLevel: '@',
+        toHeadingLevel: '@'
+      },
+      link: function(scope, el, attr, vm){
+        vm.init(scope, el, attr);
+      },
+      controller: controller
+    }
+    function controller($compile, mdService){
+      this.init = function(scope, el, attr){
+        var headingArray = TOC.createHeadingArray(
+          angular.element(scope.startHeadingElement)[0],
+          scope.fromHeadingLevel,
+          scope.toHeadingLevel
+        )
+        if(headingArray){
+          TOC.addRelationShip(headingArray);
+          var toc = TOC.toTocUi(headingArray);
+          el.html(toc)
+        }
+      }
+    }
+  }
+})();
